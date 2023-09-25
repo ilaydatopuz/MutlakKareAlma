f563ac7cbbff38d80d71d9054b070f0ea1fba874
ilaydatopuz <ilayda_topuz@yahoo.com>
2023-09-25 16:48:05 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Sayıları girin (örnek: 56 45 68 77): ");
+        string input = Console.ReadLine();
+
+        string[] numbers = input.Split(' ');
+
+        int sumOfDifferences = 0;
+        int sumOfSquaredDifferences = 0;
+
+        foreach (var number in numbers)
+        {
+            if (int.TryParse(number, out int num))
+            {
+                int difference = Math.Abs(67 - num);
+
+                if (num < 67)
+                {
+                    sumOfDifferences += difference;
+                }
+                else
+                {
+                    sumOfSquaredDifferences += difference * difference;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz giriş.");
+                return;
+            }
+        }
+
+        Console.WriteLine("Output:");
+        Console.WriteLine("Toplam farklar: " + sumOfDifferences);
+        Console.WriteLine("Toplam mutlak kare farklar: " + sumOfSquaredDifferences);
+    }
+}
